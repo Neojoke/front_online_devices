4e3f964c4618a562969712b5d702556139f0bb86
吴鹏 <wupeng15890066265@gmail.com>
2023-03-27 07:25:41 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,36 +4,40 @@
  * @LastEditTime: 2023-03-16 17:57:37
  * @FilePath: /tailwindcss_life/src/main.js
  */
-import { createApp, provide, ref } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { WS_CONNECTIONOR } from './wb'
-import './assets/main.css'
-import _ from 'lodash'
+import { createApp, provide, ref } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { WS_CONNECTIONOR } from "./wb";
+import "./assets/main.css";
+import _ from "lodash";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
-const devicesInfo = ref({})
-app.provide("devicesInfo", devicesInfo)
+app.mount("#app");
+const devicesInfo = ref({});
+app.provide("devicesInfo", devicesInfo);
 
-WS_CONNECTIONOR.init("ws://localhost:6969?from=pc")
+WS_CONNECTIONOR.init("wss://3jq6k4-6969.csb.app?from=pc");
 WS_CONNECTIONOR.subject.subscribe({
-    next: msg => {
-        if (msg.method && msg.method === "appInfo" && _.isArray(msg.params)){
-            let di = _.reduce(msg.params, (obj, item)=>{
-                if (_.isObject(item.appInfo) && _.isObject(item.serviceInfo)){
-                    item.updateTime = Date.now()
-                    obj[item.appInfo.deviceId] = item
-                }
-                return obj
-            }, {})
-            console.info('123', di)
-            devicesInfo.value = di
-        }
-    },
-    error: err => { },
-    complete: ()=> {}
-})
+  next: (msg) => {
+    if (msg.method && msg.method === "appInfo" && _.isArray(msg.params)) {
+      let di = _.reduce(
+        msg.params,
+        (obj, item) => {
+          if (_.isObject(item.appInfo) && _.isObject(item.serviceInfo)) {
+            item.updateTime = Date.now();
+            obj[item.appInfo.deviceId] = item;
+          }
+          return obj;
+        },
+        {}
+      );
+      console.info("123", di);
+      devicesInfo.value = di;
+    }
+  },
+  error: (err) => {},
+  complete: () => {},
+});
